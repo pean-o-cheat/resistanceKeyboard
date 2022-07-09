8a8f624b882f33c11c6c3091bec6860f9f0001e3
pean-o-cheat <funf33l@gmail.com>
2022-07-09 23:18:20 +0200

    fixed bug with single click after double

--- resistanceButtons.ino
+++ resistanceButtons.ino
@@ -4,18 +4,18 @@
 
 // *CONFIGURATION*
 #define BUTTONS_TOLERANCE 15
-#define LONG_PRESS_TIME 1000
-#define DOUBLE_CLICK_INTERVAL 300
+#define LONG_PRESS_TIME 800
+#define DOUBLE_CLICK_INTERVAL 80
 #define READ_DELAY 20
 #define VERBOSE true
 
 const int BUTTONS[5][4] = {
-  //{analogValue, single click function, double click function, long click function}
+    //{analogValue, single click function, double click function, long click function}
     {872, 2, 12, 20}, // CH+
     {825, 3, 13, 17}, // CH-
     {767, 7, 18, 10}, // VOL+
     {697, 6, 16, 10}, // VOL-
-    {980, 4, 11, 4}  // MODE
+    {980, 4, 11, 4}   // MODE
 };
 const int FUNCTIONS[21][2] = {
     {218},      // 0  arrow up
@@ -31,7 +31,7 @@ const int FUNCTIONS[21][2] = {
     {128, 204}, // 10 toggle mute
     {'h'},      // 11 home
     {'p'},      // 12 phone, answer call
-    {'h'},      // 13 hang up call
+    {'o'},      // 13 hang up call
     {'x'},      // 14 play
     {'c'},      // 15 pause
     {'v'},      // 16 previous track
@@ -48,7 +48,7 @@ int pressDuration = 0;
 
 // functions
 
-void printLog(char* message, int parameter)
+void printLog(char *message, int parameter)
 {
     if (VERBOSE)
     {
@@ -72,9 +72,9 @@ int readPressedButton(int analogValue)
     return -1;
 }
 
-int pressTime(int pressedButton)
+unsigned long pressTime(int pressedButton)
 {
-    int pressedTimeStart = millis();
+    unsigned long pressedTimeStart = millis();
     int currentPressedButton = pressedButton;
     int analogReadValue = 0;
     while (currentPressedButton == pressedButton)
@@ -83,7 +83,7 @@ int pressTime(int pressedButton)
         analogReadValue = analogRead(A0);
         currentPressedButton = readPressedButton(analogReadValue);
     }
-    int pressedTime = millis() - pressedTimeStart;
+    unsigned long pressedTime = millis() - pressedTimeStart;
     printLog("Wcisnieto przycisk", pressedButton);
     return pressedTime; // return pressed time
 }
@@ -91,33 +91,45 @@ int pressTime(int pressedButton)
 int detectAction(int analogReadValue)
 {
     int pressedButton = readPressedButton(analogReadValue);
+    int currentPressedButton = pressedButton;
     int action = -1;
     int pressTimeValue = pressTime(pressedButton);
+    bool doubleClicked = false;
     printLog("Czas wcisniecia", pressTimeValue);
-    int pressTimeEnd = millis();
-    if (pressedButton == -1) {return -1;}
+    unsigned long pressTimeEnd = millis();
+    if (pressedButton == -1)
+    {
+        return -1;
+    }
     if (pressTimeValue >= LONG_PRESS_TIME)
     {
         action = BUTTONS[pressedButton][3];
         printLog("Long press", action);
         return action;
-    } else
+    }
+    else
     {
-        int currentPressedButton = pressedButton;
         while (millis() - pressTimeEnd < DOUBLE_CLICK_INTERVAL)
         {
+            delay(READ_DELAY);
             currentPressedButton = readPressedButton(analogRead(A0));
-            printLog("currentButton", currentPressedButton);
             if (currentPressedButton == pressedButton)
             {
                 action = BUTTONS[pressedButton][2];
+                doubleClicked = true;
                 printLog("Double click", action);
+                while (currentPressedButton == pressedButton)
+                {
+                    currentPressedButton = readPressedButton(analogRead(A0));
+                }
                 return action;
             }
         }
+    }
+    if (!doubleClicked)
+    {
         action = BUTTONS[pressedButton][1];
         printLog("Single click", action);
-        return action;
     }
     return action;
 }
